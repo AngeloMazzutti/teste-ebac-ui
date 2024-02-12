7f1ea4d6e02e84761295dce78856737e8bcf2088
Angelo Mazzutti Verardo <angelomazzuttiv@gmail.com>
2024-02-12 16:02:07 -0300

    primeiro teste automatizado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Login', () => {
+
+    it('Deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('angelo.teste@teste.com.br')
+        cy.get('#password').type('120692')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, angelo.teste (não é angelo.teste? Sair)')
+    })
+})
