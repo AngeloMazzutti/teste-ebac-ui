277c1de27bc3d64b1fc2e18470715312698d6972
Angelo Mazzutti Verardo <angelomazzuttiv@gmail.com>
2024-02-16 15:30:58 -0300

    Page Objects

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,20 +1,31 @@
 /// <reference types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page";
+
 
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl()
     });
 
     it('Deve solucionar um produto da lista', () => {
-        cy.get('.product-block')
-        //.first()
-        //.last()
-        //.eq(2)
-        .contains('Ariel Roll Sleeve Sweatshirt')
-        .click()
+        produtosPage.buscarProdutoLista('Beaumont Summit Kit')
 
         cy.get('#tab-description > :nth-child(1)').should('contain', 'Descrição')
+        
+    });
+
+    it.only('Deve buscar um produto com sucesso', () => {
+        let produto = 'Zeppelin Yoga Pant'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto)
+    });
+
+    it('Deve visitar a página do produto', () => {
+        
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
         
     });
 });
